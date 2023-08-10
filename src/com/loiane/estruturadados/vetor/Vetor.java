b46f4c2ccd6d5aba9d001fa7e61b27fcbcac2992
Leonardo Ferreira <cadleorogfer@gmail.com>
2023-08-10 17:40:34 -0300

    add new element in array method

--- src/com/loiane/estruturadados/vetor/Vetor.java
+++ src/com/loiane/estruturadados/vetor/Vetor.java
@@ -19,14 +19,28 @@ public class Vetor {
         }
         return false;
     }
+    public boolean adiciona(int posicao, String elemento){
+        if (!(posicao >= 0 && posicao < tamanho)){
+            throw new IllegalArgumentException("Posição Inválida");
+        }
+        for (int i = this.tamanho-1; i>=posicao; i--){
+            this.elementos[i+1] = this.elementos[i];
+        }
+        this.elementos[posicao] = elemento;
+        this.tamanho++;
+        
+        return true ;
+    }
     
     public String busca(int posicao){
-        if (!(posicao > 0 && posicao < tamanho)){
+        if (!(posicao >= 0 && posicao < tamanho)){
             throw new IllegalArgumentException("Posição Inválida");
 
         }
         return this.elementos[posicao];
     }
+
+    
 
     public int tamanho(){
         return this.tamanho;
